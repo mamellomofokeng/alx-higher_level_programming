1515d0ef1f5f7b98c3c05016672451be60fb33fe
mamellomofokeng <mamellobridgettemofokeng@gmail.com>
2023-08-15 20:10:04 -0700

    Update

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -2,7 +2,6 @@
 
 #define LISTS_H
 
-
 #include <stdio.h>
 
 #include <stdlib.h>
@@ -26,20 +25,20 @@ typedef struct listint_s
 
 {
 
-        int n;
+  int n;
 
-        struct listint_s *next;
+  struct listint_s * next;
 
-} listint_t;
+}
+listint_t;
 
+size_t print_listint(const listint_t * h);
 
-size_t print_listint(const listint_t *h);
+listint_t * add_nodeint_end(listint_t ** head,
+  const int n);
 
-listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t * head);
 
-void free_listint(listint_t *head);
-
-int is_palindrome(listint_t **head);
-
+int is_palindrome(listint_t ** head);
 
 #endif /* LISTS_H */
